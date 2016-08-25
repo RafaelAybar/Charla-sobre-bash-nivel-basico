f40922c99b055768ae3e761e53164d2eb4cf3efa
Jorge <jorgesumle@freakspot.net>
2016-08-25 03:59:42 +0200

    Corregidos algunos de los errores.

--- ejemplowhile.sh
+++ ejemplowhile.sh
@@ -1,39 +1,39 @@
-#!/bin/bash 
+#!/bin/bash
 while true
 do
-	echo MENÚ DEL DÍA
-	echo 1 Pastel de carne de primero y lentejas de segundo
-	echo 2 Sopa de pescado de primero y migas de segundo
-	echo 3 Bogavante a las finas hierbas de primero y ensalada de segundo
+
+	echo "MENÚ DEL DÍA"
+	echo "1 Pastel de carne de primero y lentejas de segundo"
+	echo "2 Sopa de pescado de primero y migas de segundo"
+	echo "3 Bogavante a las finas hierbas de primero y ensalada de segundo"
 	read respuesta
 
-		if [ $respuesta = 1 ]
-			then 
-				echo ¿En serio, buena suerte?
-			else
+    if [ -z $respuesta ] # esto comprueba que la variable NO esté vacía
+    then
+        echo "Debes escoger una opción válida."
+        continue # Si no, intentará comparar '' con un número y fallará.
+    fi
 
-		if [ $respuesta = 2 ]
-				then 
-				echo ¡Muy buena elección, es nuestra especialidad¡
-			else
-		if [ $respuesta = 3 ]
-			then 
-				echo Una elección digna de un auténtico sibarita...
-			else
-		if [ -z $respuesta ] #esto comprueba que la variable NO esté vacía
-			then 
-				echo Debes escoger una opción válida.
-	
-		if [ $respuesta -ge 3 ] #esto comprueba que la variable NO sea mayor que 3
-			then
-				
-				echo Debe ser uno de los números del menú
-			
-		
+    # Para Bash verdadero es igual a 1 y falso es igual a 0.
+    # Si la condición es verdadera, Bash devuelve 1 y se ejecuta el código dentro del if
+    if [ $respuesta = 1 ]
+    then
+        echo "¿En serio, buena suerte?"
 
-		fi
-		fi
-		fi
-		fi
-		fi	
+    # elif solo comprobará su condición cuando la condición del if devuelva 0.
+    # Si la respuesta es 1, no hace falta comprobar si es 2.
+    elif [ $respuesta = 2 ]
+    then
+        echo "¡Muy buena elección, es nuestra especialidad¡"
+    elif [ $respuesta = 3 ]
+    then
+        echo "Una elección digna de un auténtico sibarita..."
+    fi
+
+    if [ $respuesta -gt 3 ] # esto comprueba que la variable NO sea mayor que 3
+    then
+        echo "Debe ser uno de los números del menú."
+    fi
+
 done
+
