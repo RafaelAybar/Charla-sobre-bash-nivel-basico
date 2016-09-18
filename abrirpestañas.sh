13260d2b8ae1301002d52093a527fe532f46a415
RafaelAybar <rafaelaybar@hotmail.com>
2016-09-18 16:12:26 +0200

    Gracias Jorge

---
+++ abrirpestañas.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+firefox "https://es5.forgeofempires.com/game/index?ref=" "http://www.elalmeria.es/" "http://www.ideal.es/almeria/" "http://www.eldiario.es/" "http://www.elconfidencial.com/" "http://www.20minutos.es/" "http://vozpopuli.com/inicio" "http://www.elplural.com/" "http://www.infolibre.es/" "https://www.google.es/?client=firefox-b#q=rafa+es+genial&gfe_rd=cr"
