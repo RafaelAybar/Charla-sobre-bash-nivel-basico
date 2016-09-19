a009f3ae3e63a6220196b7a050ee92461f57506f
Jorge <jorgesumle@freakspot.net>
2016-09-19 20:14:15 +0200

    Eliminados los espacios a final de línea en ejemplofor.sh

--- ejemplofor.sh
+++ ejemplofor.sh
@@ -2,15 +2,15 @@ echo "Vamos a comprobar cuántos ficheros y directorios tienes"
 for fichero in $(ls .)
  		#Con esto hacemos que en nuestra carpeta, el bucle for detecte qué es lo que contiene nuestra carpeta
  do
-	if [ -d $fichero ] 
+	if [ -d $fichero ]
 		#comprueba que es un directorio
 		then
-			
+
 		echo "Este es el directorio $fichero"
 	elif [ -f $fichero ]
 		#comprueba que es un fichero
 		then
-			
+
 			echo "Este es el fichero $fichero"
 	fi
  done
